188218ab2a360ac88745b6a5d3baff134530a999
Jessey Bullock <jbullock@isecpartners.com>
2014-08-10 01:43:34 -0500

    moar fixing

--- sslsocket/src/servers/clientAuth.java
+++ sslsocket/src/servers/clientAuth.java
@@ -4,22 +4,26 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.OutputStreamWriter;
+import java.net.SocketAddress;
 import java.security.KeyStore;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
-import java.util.Arrays;
 
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLHandshakeException;
 import javax.net.ssl.SSLServerSocket;
 import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.ssl.SSLSession;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.TrustManagerFactory;
 import javax.net.ssl.X509TrustManager;
+
 import javax.xml.bind.DatatypeConverter;
 
 
@@ -39,27 +43,30 @@ public class clientAuth {
 		try {
 			
 			//password for keystore
-			char ksPass[] = "password".toCharArray();
+			char keyPass[] = "password".toCharArray();
 			//password for private key in keystore
 			char privPass[] = "Testing123!".toCharArray();
 			//path of my keystore
-			String ksName = "/root/mycerts/keystore.jks";
+			String keyPath = "/root/mycerts/keystore.jks";
 			
 			//create and load a keystore
 			KeyStore ks = KeyStore.getInstance("JKS");
-			ks.load(new FileInputStream(ksName), ksPass);
+			ks.load(new FileInputStream(keyPath), keyPass);
 			
 			//create and init the KeyManager
 			KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
 			kmf.init(ks, privPass);
 			
 			//create our custom TrustManager for cert pinning
-			TrustManager[] myTMs = new TrustManager [] {
-					new MyX509TrustManager() };
+			TrustManager[] fpm = new TrustManager [] {
+					new FingerprintManager() };
+			
+			//explicitly specifying the securerandom to pass into the SSLContext
+			SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG");
 			
 			SSLContext sc = SSLContext.getInstance("TLS");
 			//initialize the SSLContext with the keymanager (server certs) and our custom Trustmanager for cert pinning
-			sc.init(kmf.getKeyManagers(), myTMs, null);
+			sc.init(kmf.getKeyManagers(), fpm, secureRandom);
 			//setup the ssl socket server
 			SSLServerSocketFactory ssf = sc.getServerSocketFactory();
 			SSLServerSocket s = (SSLServerSocket) ssf.createServerSocket(443);
@@ -67,8 +74,7 @@ public class clientAuth {
 			s.setNeedClientAuth(true);
 			//accept a connection
 			SSLSocket c = (SSLSocket) s.accept();
-			
-			
+	
 			//http://stackoverflow.com/questions/10687200/java-7-and-could-not-generate-dh-keypair
 			//This is mainly a fix for working with openssl s_client
 			c.setEnabledCipherSuites(new String[] {
@@ -82,8 +88,10 @@ public class clientAuth {
 			        "SSL_DHE_DSS_WITH_3DES_EDE_CBC_SHA",
 			        });
 			
-			BufferedWriter w = new BufferedWriter(new OutputStreamWriter(c.getOutputStream()));
+			SocketAddress remoteAddr = c.getRemoteSocketAddress();
+			System.out.println("[*] Recieved connection from: " + remoteAddr.toString().substring(1));
 			
+			BufferedWriter w = new BufferedWriter(new OutputStreamWriter(c.getOutputStream()));
 			String m = "[*] Authenticated your certificate, Welcome to the NCC Test SSL Socket Server";
 			w.write(m,0,m.length());
 			w.newLine();
@@ -144,38 +152,39 @@ public class clientAuth {
 		
 		System.out.println("[*] Recieved Client Cert from: " + cert.getSubjectDN());
 		
-		MessageDigest md;
-		try {
-			md = MessageDigest.getInstance("SHA-256");
-			md.reset();
-		    fingerPrint = md.digest(cert.getEncoded());
-		    
-		    System.out.println("[*] Client Certificate Fingerprint:\t" + DatatypeConverter.printHexBinary(fingerPrint));
-		    System.out.println("[*] Allowed Certificate Fingerprint:\t" + DatatypeConverter.printHexBinary(allowedFinger));
-		    
-		}catch (Exception e) 
+		MessageDigest md = null;
+
+		try 
 		{
+			md = MessageDigest.getInstance("SHA-256");
+		}catch (NoSuchAlgorithmException e) {
 			e.printStackTrace();
 		}
-		    //see if the cert fingerprints match
-		    if (Arrays.equals(fingerPrint, allowedFinger))
-		    {
-		    	System.out.println("[*] Fingerprints matched!!!!!");
-		    	return true;
-		    	
-		    } else 
-		    {
-		    	throw new CertificateException("client certificate with unknown fingerprint: " + cert.getSubjectDN());
-		    }
+		
+		fingerPrint = md.digest(cert.getEncoded());
+		md.reset();
+	    
+		System.out.println("[*] Client Certificate Fingerprint:\t" + DatatypeConverter.printHexBinary(fingerPrint));
+		System.out.println("[*] Allowed Certificate Fingerprint:\t" + DatatypeConverter.printHexBinary(allowedFinger));
+		    
+		//see if the cert fingerprints match
+		if (md.isEqual(fingerPrint, allowedFinger))
+		{
+			System.out.println("[*] Fingerprints matched!!!!!");
+			return true;	
+		}else 
+		{
+			throw new CertificateException("client certificate with unknown fingerprint: " + cert.getSubjectDN());
+		}
 	}
 	
 	//http://docs.oracle.com/javase/7/docs/technotes/guides/security/jsse/JSSERefGuide.html#X509TrustManager
-	public static class MyX509TrustManager implements X509TrustManager {
+	public static class FingerprintManager implements X509TrustManager {
 		
-		private MyX509TrustManager() throws Exception {
+		private FingerprintManager() throws Exception {
 			// create a "default" JSSE X509TrustManager so we can call it for stuff we don't implement
 
-	    	//create an emtpy keystore since we aren't really using it
+	    	//create an empty keystore since we aren't really using it
 	        KeyStore ks = KeyStore.getInstance("JKS");
 	        ks.load(null, null);
 
@@ -206,22 +215,23 @@ public class clientAuth {
 	         throw new Exception("[*] Couldn't initialize");
 	     }
 
-	     //This is where we implement our custom certificate pinning logic
-	     public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException 
-	     {
-	             checkFingerprint(chain); 
-	     }
+		//This is where we implement our custom certificate pinning logic
+		public void checkClientTrusted(X509Certificate[] chain, String authType) 
+				throws CertificateException 
+		{
+			checkFingerprint(chain); 
+		}
 
-	     //we aren't doing server side checking as we are the server, so just pass this through
-	     public void checkServerTrusted(X509Certificate[] chain, String authType)
-	                 throws CertificateException {
-	    	 
-	    	 pkixTrustManager.checkServerTrusted(chain, authType);
-	     }
+	    //we aren't doing server side checking as we are the server, so just pass this through
+	    public void checkServerTrusted(X509Certificate[] chain, String authType)
+	    		throws CertificateException 
+	    {
+	    	pkixTrustManager.checkServerTrusted(chain, authType);
+	    }
 
-         //passing this through again	   
-	     public X509Certificate[] getAcceptedIssuers() {
-	         return pkixTrustManager.getAcceptedIssuers();
-	     }
+	    //passing this through again	   
+	    public X509Certificate[] getAcceptedIssuers() {
+	    	return pkixTrustManager.getAcceptedIssuers();
+	    }
 	}
 }
